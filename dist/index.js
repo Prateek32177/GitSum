9ec6c0075761d7248b571f8cfc0a2aa2c08d2c7b
Prateek Jain <Prateek32177@gmail.com>
2024-06-11 22:07:42 +0530

    experimenting with core library

--- dist/index.js
+++ dist/index.js
@@ -34,7 +34,15 @@ exports.run = void 0;
 const core = __importStar(__nccwpck_require__(2186));
 const run = () => {
     core.summary.addHeading("hello prateek").write();
-    core.summary.addQuote("hkjhjhkj");
+    core.summary
+        .addQuote("hkjhjhkj")
+        .addBreak()
+        .addCodeBlock("coede block")
+        .addDetails("open details", "nice detail")
+        .addHeading("Summary headline")
+        .addImage("../images/qrimage.png", "QR code")
+        .addLink("DeazyQR", "https://deazyqr.vercel.app/").addRaw("<h1>Hi prateek</h1><pre>Looks great</pre>").addSeparator().write();
+    core.debug("hello");
 };
 exports.run = run;
 (0, exports.run)();
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import * as core from "@actions/core"
-
-export const run = ()=>{
-core.summary.addHeading("hello prateek").write();
-core.summary.addQuote("hkjhjhkj")
-}
+import * as core from "@actions/core";
+export const run = () => {
+  core.summary.addHeading("hello prateek").write();
+  core.summary
+    .addQuote("hkjhjhkj")
+    .addBreak()
+    .addCodeBlock("coede block")
+    .addDetails("open details", "nice detail")
+    .addHeading("Summary headline")
+    .addImage("../images/qrimage.png", "QR code")
+    .addLink("DeazyQR", "https://deazyqr.vercel.app/").addRaw("<h1>Hi prateek</h1><pre>Looks great</pre>").addSeparator().write();
+  core.debug("hello");
+};
 
 run();
