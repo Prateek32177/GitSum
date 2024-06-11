50d71f8837d24db2a59c8fcc34a3e4a52b9666de
Prateek Jain <Prateek32177@gmail.com>
2024-06-11 22:22:09 +0530

    html addition

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,24 @@ export const run = () => {
     .addDetails("open details", "nice detail")
     .addHeading("Summary headline")
     .addImage("../images/qrimage.png", "QR code")
-    .addLink("DeazyQR", "https://deazyqr.vercel.app/").addRaw("<h1>Hi prateek</h1><pre>Looks great</pre>").addSeparator().write();
+    .addLink("DeazyQR", "https://deazyqr.vercel.app/").addRaw(html).addSeparator().write();
+    core.setOutput("html summary",html);
   core.debug("hello");
 };
 
 run();
+
+
+const html = `<div><h1>GitHub Action Workflow Summary</h1>
+<div style='margin-bottom: 10px; padding: 10px; border: 1px solid #ccc; background-color: #f8f8f8;'>
+  <p style='color: #333;'>This is a summary of the GitHub Action workflow.</p>
+  <div style='background-color: #fdd; padding: 5px; margin-bottom: 5px;'>
+    <p style='color: #900;'>Error: Something went wrong.</p>
+  </div>
+  <div style='background-color: #ffecb3; padding: 5px; margin-bottom: 5px;'>
+    <p style='color: #f57c00;'>Warning: Proceed with caution.</p>
+  </div>
+  <div style='background-color: #e6f7ff; padding: 5px; margin-bottom: 5px;'>
+    <p style='color: #2196f3;'>Info: This is just an informational message.</p>
+  </div>
+</div></div>`
